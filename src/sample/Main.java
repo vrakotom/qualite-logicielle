f27ed281851aa395322e5918ed6411fde87fa527
vrakotom <vrakotom@ece.fr>
2015-11-06 00:00:10 +0100

    Mise en place de la GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("cockpit.fxml"));
         primaryStage.setTitle("The Landing Gear System");
         primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
